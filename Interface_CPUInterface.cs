5eef7beb3296df1f5fc6050939e52d1686a2982e
MessyAnswer <36890421+MessyAnswer@users.noreply.github.com>
2018-04-03 21:34:21 +0200

    remove unnecessary file

--- Interface_CPUInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Interface
-{
-    namespace CPUInterface
-    {
-        class Classname { // Up to you, messyanswer!
-            string something = Constants.PIECE_REPRESENTATIONS(-1); // Referencing constants works
-        }
-    }
-}
