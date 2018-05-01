e03a8d988fc79787802d1ec18f9b2d6dc38bb79c
MessyAnswer <36890421+MessyAnswer@users.noreply.github.com>
2018-05-01 17:06:25 +0200

    make sure there's a Main function

---
+++ Interface_Main.cs
@@ -0,0 +1,32 @@
+using ChessEngine;
+
+namespace Interface
+{
+    class MainIO
+    {
+        static void Main()
+        {
+            Engine engine = new Engine();
+            UCIProtocol.engine = engine;
+            UCIProtocol.InputLoop();
+
+            /*
+            Position a = new Position();
+
+            System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+
+
+            for (int c = 2; c <= 2; c++)
+            {
+                watch.Start();
+                Console.WriteLine(string.Format("Evaluation: {0}", a.FindBestMove(c, -2, 2)));
+                watch.Stop();
+
+                Console.WriteLine(string.Format("Depth: {0} | Elapsed milliseconds: {1}", c, watch.ElapsedMilliseconds));
+                watch.Reset();
+            }
+            Console.ReadKey();
+            */
+        }
+    }
+}
