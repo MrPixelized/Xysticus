2b6c6ef7570815b35083e457d52f9ca0ab29553f
MessyAnswer <36890421+MessyAnswer@users.noreply.github.com>
2018-05-10 19:13:09 +0200

    bugfixes and small improvements
    The engine should be pretty much working now.

--- Chess_Engine.cs
+++ Chess_Engine.cs
@@ -35,18 +35,19 @@ namespace Chess
                     //Console.WriteLine(String.Format("{0}White here, now looking into {1}, {2} to {3}, {4}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY));
 
                     // If the requested depth has not yet been reached, generate another layer of positions
-                    if (depth == 0)
+                    nextPositionMoveTupleList = position.GeneratePositions();
+                    if (nextPositionMoveTupleList.Count == 0)
                     {
                         moveEvaluationTupleList.Add((move, (float)evaluationFunction.NextDouble()));
+                        //Console.WriteLine(String.Format("{0}The value we're adding to {1}, {2} to {3}, {4} is {5}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY, moveEvaluationTupleList.Last().Item2));
                     }
-                    else
+                    else if (nextPositionMoveTupleList.Last().Item1 != null)
                     {
-                        nextPositionMoveTupleList = position.GeneratePositions();
-                        if (nextPositionMoveTupleList.Count == 0)
+                        if (depth <= 0)
                         {
                             moveEvaluationTupleList.Add((move, (float)evaluationFunction.NextDouble()));
                         }
-                        else if (nextPositionMoveTupleList.Last().Item1 != null)
+                        else
                         {
                             moveEvaluationTupleList.Add((move, FindBestMove(position, nextPositionMoveTupleList, depth - 1, alpha, beta).Item2));
                         }
@@ -59,12 +60,12 @@ namespace Chess
                         alpha = Math.Max(bestEvaluation, alpha);
                         if (beta <= alpha) break;
                     }
-                    if (moveEvaluationTupleList.Count == 0)
-                    {
-                        //This only occurs when we're in a position where there are pseudo-legal moves, but no legal moves.
-                        //In this case, it's safe to return a random move and the evaluation for the final position.
-                        return (positionMoveTupleList.Last().Item2, (float)evaluationFunction.NextDouble());
-                    }
+                }
+                if (moveEvaluationTupleList.Count == 0)
+                {
+                    //This only occurs when we're in a position where there are pseudo-legal moves, but no legal moves.
+                    //In this case, it's safe to return a random move and the evaluation for the final position.
+                    return (positionMoveTupleList.Last().Item2, (float)evaluationFunction.NextDouble());
                 }
                 moveEvaluationTupleList.Sort((x, y) => y.Item2.CompareTo(x.Item2));
             }
@@ -74,39 +75,40 @@ namespace Chess
                 bestEvaluation = 100;
                 foreach ((Position position, Move move) in positionMoveTupleList)
                 {
-                    //Console.WriteLine(String.Format("{0}White here, now looking into {1}, {2} to {3}, {4}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY));
+                    //Console.WriteLine(String.Format("{0}Black here, now looking into {1}, {2} to {3}, {4}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY));
 
                     // If the requested depth has not yet been reached, generate another layer of positions
-                    if (depth == 0)
+                    nextPositionMoveTupleList = position.GeneratePositions();
+                    if (nextPositionMoveTupleList.Count == 0)
                     {
                         moveEvaluationTupleList.Add((move, (float)evaluationFunction.NextDouble()));
+                        //Console.WriteLine(String.Format("{0}The value we're adding to {1}, {2} to {3}, {4} is {5}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY, moveEvaluationTupleList.Last().Item2));
                     }
-                    else
+                    else if (nextPositionMoveTupleList.Last().Item1 != null) //if the king cannot be captured in position
                     {
-                        nextPositionMoveTupleList = position.GeneratePositions();
-                        if (nextPositionMoveTupleList.Count == 0)
+                        if (depth <= 0)
                         {
                             moveEvaluationTupleList.Add((move, (float)evaluationFunction.NextDouble()));
                         }
-                        else if (nextPositionMoveTupleList.Last().Item1 != null)
+                        else
                         {
                             moveEvaluationTupleList.Add((move, FindBestMove(position, nextPositionMoveTupleList, depth - 1, alpha, beta).Item2));
                         }
                         //Console.WriteLine(String.Format("{0}The value we're adding to {1}, {2} to {3}, {4} is {5}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY, moveEvaluationTupleList.Last().Item2));
                     }
-                    // MINIMAX
+                    //MINIMAX
                     if (moveEvaluationTupleList.Count != 0)
                     {
                         bestEvaluation = Math.Min(bestEvaluation, moveEvaluationTupleList.Last().Item2);
                         beta = Math.Min(bestEvaluation, beta);
                         if (beta <= alpha) break;
                     }
-                    if (moveEvaluationTupleList.Count == 0)
-                    {
-                        //This only occurs when we're in a position where there are pseudo-legal moves, but no legal moves.
-                        //In this case, it's safe to return a random move and the evaluation for the final position.
-                        return (positionMoveTupleList.Last().Item2, (float)evaluationFunction.NextDouble());
-                    }
+                }
+                if (moveEvaluationTupleList.Count == 0)
+                {
+                    //This only occurs when we're in a position where there are pseudo-legal moves, but no legal moves.
+                    //In this case, it's safe to return a random move and the evaluation for the final position.
+                    return (positionMoveTupleList.Last().Item2, (float)evaluationFunction.NextDouble());
                 }
                 moveEvaluationTupleList.Sort((x, y) => x.Item2.CompareTo(y.Item2));
             }
@@ -118,3 +120,4 @@ namespace Chess
         }
     }
 }
+ 
