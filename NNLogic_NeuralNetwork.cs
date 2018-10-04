3a8c013a26dd19bf57b19041bb137bd02bef1f17
MessyAnswer <36890421+MessyAnswer@users.noreply.github.com>
2018-10-04 16:20:21 +0200

    more work on NeuralNetwork
    With the tests I've done, it seems the neural network is fully functional now.

--- NNLogic_NeuralNetwork.cs
+++ NNLogic_NeuralNetwork.cs
@@ -5,25 +5,29 @@ namespace NNLogic
 {
     class NeuralNetwork
     {
-        public float[,] weights;
+        public float[][] weights;
         readonly int hiddenLayerCount;
         readonly int inputNodeCount;
         readonly int hiddenNodeCount;
         readonly int outputNodeCount;
 
-        public NeuralNetwork(int hiddenLayerCount, int inputNodeCount, int hiddenNodeCount, int outputNodeCount, float[,] weights) {
+        public NeuralNetwork(int hiddenLayerCount, int inputNodeCount, int hiddenNodeCount, int outputNodeCount, float[][] weights) {
             this.hiddenLayerCount = hiddenLayerCount;
             this.inputNodeCount = inputNodeCount;
             this.hiddenNodeCount = hiddenNodeCount;
             this.outputNodeCount = outputNodeCount;
             this.weights = weights;
         }			
-        public float[] CalculateResult()
+        public float[] CalculateResult(float[] activations)
         {
-            float[] output = { 0.1f };
-            return output;
+            float[] currentActivations = activations;
+            for (int i = 0; i <= hiddenLayerCount; i++)
+            {
+                currentActivations = _advance(currentActivations, i);
+            }
+            return currentActivations;
         }
-        public float[] _advance(float[] activations, int layer)
+        private float[] _advance(float[] activations, int layer)
         {
             // The node count of this layer and that of the previous is dependent on the layer we are calculating the activations of.
             int thisLayerNodeCount = (layer > hiddenLayerCount) ? outputNodeCount : hiddenNodeCount;
@@ -39,16 +43,18 @@ namespace NNLogic
                 {
                     // j represents the index of the neuron of which we are currently using the activation.
                     // The index of connection from j to i is: i * (number of nodes in the previous layer) + j
-                    nextActivations[i] += weights[layer, i * (previousLayerNodeCount + 1) + j] * activations[j];
+                    nextActivations[i] += weights[layer][i * (previousLayerNodeCount + 1) + j] * activations[j];
                 }
-                //Add the bias
-                nextActivations[i] += weights[layer, (i + 1) * (previousLayerNodeCount + 1) - 1];
+                // Add the bias
+                nextActivations[i] += weights[layer][(i + 1) * (previousLayerNodeCount + 1) - 1];
+                // Apply the activation function
+                nextActivations[i] = _activation(nextActivations[i]);
             }
             return nextActivations;
         }
         private float _activation(float activation)
         {
-            return (activation > 0) ? activation : 0;
+            return Math.Max(activation, 0);
         }
     }
 }
