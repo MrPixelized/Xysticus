44db35bd55746c57eef64bfeb778190c5a3e810d
MessyAnswer <36890421+MessyAnswer@users.noreply.github.com>
2018-10-09 21:23:05 +0200

    start work on training
    What has been done so far:
    • _pickNets() has been completed (picks nets according to their fitness, i.e. the probability a network is picked scales linearly with its fitness);
    • GenerateNextGeneration() has been written completely, but _crossover(), a function it depends on, has not.
    
    What still needs to be done:
    • Come up with a good way to perform crossover and implement it;
    • Come up with a good way to reliably calculate fitness and implement it;
    • Come up with a good way to store results of a training session, such that the program can continue with it later.

--- NNLogic_Training.cs
+++ NNLogic_Training.cs
@@ -1,7 +1,76 @@
+using System;
+using System.Collections.Generic;
+
 namespace NNLogic
 {
     public class Training
     {
+        public NeuralNetwork[] population;
+        public float[] fitnesses;
+        int generation;
+        int reproductionOrganismCount;
+        Random random;
 
+        public Training(NeuralNetwork[] population, float[] fitnesses, int reproductionOrganismCount)
+        {
+            this.population = population;
+            this.fitnesses = fitnesses;
+            this.reproductionOrganismCount = reproductionOrganismCount;
+            random = new Random();
+        }
+
+        public void Train()
+        {
+            // RateNets() - the function that is responsible for assigning fitnesses to the nets.
+            // GenerateNextGeneration() - the function that is responsible for making the next generation.
+            // Then just loop it, and we're done :).
+        }
+
+        public void RateNets()
+        {
+
+        }
+
+        public void GenerateNextGeneration()
+        {
+            float fitnessSum = 0;
+            for (int i = 0; i < fitnesses.Length; i++)
+            {
+                fitnessSum += fitnesses[i];
+            }
+
+            List<NeuralNetwork> nextPopulation = new List<NeuralNetwork>();
+            for (int i = 0; i < population.Length; i++)
+            {
+                // Generate a new individual
+                List<NeuralNetwork> recombinationList = _pickNets(fitnessSum);
+                nextPopulation.Add(_crossover(recombinationList));
+            }
+            population = nextPopulation.ToArray();
+        }
+
+        private List<NeuralNetwork> _pickNets(float fitnessSum)
+        {
+            List<NeuralNetwork> recombinationList = new List<NeuralNetwork>();
+            for (int i = 0; i < reproductionOrganismCount; i++)
+            {
+                float fitnessIndex = (float)random.NextDouble() * fitnessSum;
+                float temporaryFitnessSum = 0;
+                for (int j = 0; j < population.Length; j++)
+                {
+                    temporaryFitnessSum += fitnesses[j];
+                    if (fitnessSum <= temporaryFitnessSum)
+                    {
+                        recombinationList.Add(population[j]);
+                    }
+                }
+            }
+            return recombinationList;
+        }
+
+        private NeuralNetwork _crossover(List<NeuralNetwork> netsToCombine)
+        {
+            return netsToCombine[0];
+        }
     }
 }
