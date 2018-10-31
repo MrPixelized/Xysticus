0484c8a528ddeaf550fb537838967623fb71d936
MessyAnswer <36890421+MessyAnswer@users.noreply.github.com>
2018-10-31 18:08:42 +0100

    start work on Chess.Game
    Chess.Game is the class used for letting two neural networks play a game against each other. NNLogic.Training.RunRoundRobin() will repeatedly make use of this class.

--- Chess_Game.cs
+++ Chess_Game.cs
@@ -1,15 +1,30 @@
+using NNLogic;
+using System;
+
 namespace Chess
 {
     public class Game
     {
         public Position currentPosition;
+        public NeuralNetwork whitePlayer;
+        public NeuralNetwork blackPlayer;
 
-        public Game(Position currentPosition = null)
+        public Game()
         {
-            if (currentPosition == null) {
-                currentPosition = new Position();
-            }
-            this.currentPosition = currentPosition;
+            currentPosition = new Position();
         }        
+        public void Play()
+        {
+            (Move, float) moveEvaluationTuple;
+            while (true)
+            {
+                // Make the white player find a move and apply it to the position.
+                moveEvaluationTuple = Engine.FindBestMove(ref whitePlayer, currentPosition, 2);
+                currentPosition = currentPosition.MakeMove(moveEvaluationTuple.Item1);
+                // Make the black player find a move and apply it to the position.
+                moveEvaluationTuple = Engine.FindBestMove(ref blackPlayer, currentPosition, 2);
+                currentPosition = currentPosition.MakeMove(moveEvaluationTuple.Item1);
+            }
+        }
     }
 }
