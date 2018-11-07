a179d2a1fb00f2bbf2c470e15bb5ca4047cab303
MessyAnswer <36890421+MessyAnswer@users.noreply.github.com>
2018-11-07 18:53:24 +0100

    finish Chess.Game
    Chess.Game allows us to let two networks play against each other.

--- Chess_Game.cs
+++ Chess_Game.cs
@@ -1,30 +1,116 @@
 using NNLogic;
-using System;
+using System.Collections.Generic;
+using System.Linq;
+using Interface;
+using static Chess.Constants;
 
 namespace Chess
 {
     public class Game
     {
         public Position currentPosition;
+        public List<string> moveHistory;
         public NeuralNetwork whitePlayer;
         public NeuralNetwork blackPlayer;
+        public bool isCheck;
+        public float? result;
 
-        public Game()
+        public Game(Position startingPosition = null)
         {
-            currentPosition = new Position();
+            currentPosition = startingPosition ?? new Position();
+            moveHistory = new List<string>();
         }        
         public void Play()
         {
             (Move, float) moveEvaluationTuple;
+            List<(Position, Move)> nextPositionMoveTupleList = currentPosition.GeneratePositions();
             while (true)
             {
-                // Make the white player find a move and apply it to the position.
-                moveEvaluationTuple = Engine.FindBestMove(ref whitePlayer, currentPosition, 2);
-                currentPosition = currentPosition.MakeMove(moveEvaluationTuple.Item1);
-                // Make the black player find a move and apply it to the position.
-                moveEvaluationTuple = Engine.FindBestMove(ref blackPlayer, currentPosition, 2);
-                currentPosition = currentPosition.MakeMove(moveEvaluationTuple.Item1);
+                if (currentPosition.toMove == WHITE)
+                {
+                    // Make the white player find a move and apply it to the position.
+                    moveEvaluationTuple = Engine.FindBestMove(ref whitePlayer, currentPosition, nextPositionMoveTupleList, 2, -2.0f, 2.0f);
+                    currentPosition = currentPosition.MakeMove(moveEvaluationTuple.Item1);
+                    if (currentPosition.fiftyMoveProximity >= 100)
+                    {
+                        result = 0.5f;
+                        break;
+                    }
+                    nextPositionMoveTupleList = currentPosition.GeneratePositions();
+                    if (nextPositionMoveTupleList.Last().Item1 == null)
+                    {
+                        // White just played an illegal move (which only happens when there are no legal moves).
+                        if (isCheck)
+                        {
+                            result = -1;
+                            break;
+                        }
+                        else
+                        {
+                            result = 0.5f;
+                            break;
+                        }
+                    }
+                    isCheck = currentPosition.IsCheck();
+                    if (nextPositionMoveTupleList.Count == 0)
+                    {
+                        // White just played a move which doesn't give black any pseudo-legal moves.
+                        if (isCheck)
+                        {
+                            result = 1;
+                            break;
+                        }
+                        else
+                        {
+                            result = 0.5f;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    // Make the black player find a move and apply it to the position.
+                    moveEvaluationTuple = Engine.FindBestMove(ref whitePlayer, currentPosition, nextPositionMoveTupleList, 2, -2.0f, 2.0f);
+                    currentPosition = currentPosition.MakeMove(moveEvaluationTuple.Item1);
+                    if (currentPosition.fiftyMoveProximity >= 100)
+                    {
+                        result = 0.5f;
+                        break;
+                    }
+                    nextPositionMoveTupleList = currentPosition.GeneratePositions();
+                    if (nextPositionMoveTupleList.Last().Item1 == null)
+                    {
+                        // Black just played an illegal move (which only happens when there are no legal moves).
+                        if (isCheck)
+                        {
+                            result = 1;
+                            break;
+                        }
+                        else
+                        {
+                            result = 0.5f;
+                            break;
+                        }
+                    }
+                    isCheck = currentPosition.IsCheck();
+                    if (nextPositionMoveTupleList.Count == 0)
+                    {
+                        // Black just played a move which doesn't give white any pseudo-legal moves.
+                        if (isCheck)
+                        {
+                            result = -1;
+                            break;
+                        }
+                        else
+                        {
+                            result = 0.5f;
+                            break;
+                        }
+                    }
+                }
+                moveHistory.Add(UCIProtocol.MoveToUCINotation(moveEvaluationTuple.Item1) + " ");
             }
+            System.IO.File.WriteAllLines("movehistory.pgn", moveHistory);
         }
     }
 }
