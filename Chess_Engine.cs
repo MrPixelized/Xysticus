8a9f824eb3512b44a10e1deb396b7467d24db7f8
MessyAnswer <36890421+MessyAnswer@users.noreply.github.com>
2018-11-21 16:40:40 +0100

    update comments

--- Chess_Engine.cs
+++ Chess_Engine.cs
@@ -21,42 +21,32 @@ namespace Chess
                 foreach ((Position position, Move move) in positionMoveTupleList)
                 {
                     if (move.pieceToMove == WHITE_KING && (move.toX - move.fromX == 2 || move.toX - move.fromX == -2))
-                    //If the white king wants to castle, there is a couple things we need to check first.
+                    // If the white king wants to castle, there is a couple things we need to check first.
                     {
-                        if (!lastMoveWasLegal || currentPosition.IsCheck())
-                        {
-                            continue;
-                        }
+                        if (!lastMoveWasLegal || currentPosition.IsCheck()) continue;
                     }
-
                     lastMoveWasLegal = false;
-                    //Console.WriteLine(String.Format("{0}White here, now looking into {1}, {2} to {3}, {4}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY));
-                    //Generate positions for the second generation of child nodes from here.
-                    //This is necessary to get rid of illegal moves in the first generation of child nodes.
+                    // Generate another layer of positions
+                    // When in a leaf node, it's still important to do this because this position might be illegal.
                     nextPositionMoveTupleList = position.GeneratePositions();
                     if (nextPositionMoveTupleList.Count == 0)
                     {
-                        //After the most recent legal move, there are no pseudo-legal moves and the game has ended.
+                        // After the most recent legal move, there are no pseudo-legal moves and the game has ended.
                         moveEvaluationTupleList.Add((move, evaluationFunction.EvaluatePosition(position)));
                         lastMoveWasLegal = true;
-                        //Interface.ConsoleGraphics.DrawPosition(position);
-                        //Console.WriteLine(String.Format("{0}The value we're adding to {1}, {2} to {3}, {4} is {5}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY, moveEvaluationTupleList.Last().Item2));
-                    }
+                        }
                     else if (nextPositionMoveTupleList.Last().Item1 != null)
-                    //The most recent pseudo-legal move does not allow a king to be captured. 
+                    // The most recent pseudo-legal move does not allow a king to be captured. 
                     {
                         lastMoveWasLegal = true;
                         if (depth == 0)
                         {
                             moveEvaluationTupleList.Add((move, evaluationFunction.EvaluatePosition(position)));
-                            //Interface.ConsoleGraphics.DrawPosition(position);
                         }
                         else
                         {
                             moveEvaluationTupleList.Add((move, FindBestMove(ref evaluationFunction, position, nextPositionMoveTupleList, depth - 1, alpha, beta).Item2));
-                            //Interface.ConsoleGraphics.DrawPosition(position);
                         }
-                        //Console.WriteLine(String.Format("{0}The value we're adding to {1}, {2} to {3}, {4} is {5}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY, moveEvaluationTupleList.Last().Item2));
                     }
                     // MINIMAX
                     if (moveEvaluationTupleList.Count != 0)
@@ -68,8 +58,8 @@ namespace Chess
                 }
                 if (moveEvaluationTupleList.Count == 0)
                 {
-                    //This only occurs when we're in a position where there are pseudo-legal moves, but no legal moves.
-                    //In this case, it's safe to return a random move and the evaluation for the final position.
+                    // This only occurs when we're in a position where there are pseudo-legal moves, but no legal moves.
+                    // In this case, it's safe to return a random move and the evaluation for the final position.
                     return (positionMoveTupleList.Last().Item2, evaluationFunction.EvaluatePosition(currentPosition));
                 }
                 moveEvaluationTupleList.Sort((x, y) => y.Item2.CompareTo(x.Item2));
@@ -81,40 +71,31 @@ namespace Chess
                 foreach ((Position position, Move move) in positionMoveTupleList)
                 {
                     if (move.pieceToMove == BLACK_KING && (move.toX - move.fromX == 2 || move.toX - move.fromX == -2))
-                    //If the black king wants to castle, there is a couple things we need to check first.
+                    // If the black king wants to castle, there is a couple things we need to check first.
                     {
-                        if (!lastMoveWasLegal || currentPosition.IsCheck())
-                        {
-                            continue;
-                        }
+                        if (!lastMoveWasLegal || currentPosition.IsCheck()) continue;
                     }
-                    //Console.WriteLine(String.Format("{0}Black here, now looking into {1}, {2} to {3}, {4}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY));
-
-                    // If the requested depth has not yet been reached, generate another layer of positions
+                    // Generate another layer of positions
+                    // When in a leaf node, it's still important to do this because this position might be illegal.
                     nextPositionMoveTupleList = position.GeneratePositions();
                     if (nextPositionMoveTupleList.Count == 0)
                     {
-                        //After the most recent legal move, there are no pseudo-legal moves and the game has ended. 
+                        // After the most recent legal move, there are no pseudo-legal moves and the game has ended. 
                         moveEvaluationTupleList.Add((move, evaluationFunction.EvaluatePosition(position)));
-                        //Interface.ConsoleGraphics.DrawPosition(position);
-                        //Console.WriteLine(String.Format("{0}The value we're adding to {1}, {2} to {3}, {4} is {5}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY, moveEvaluationTupleList.Last().Item2));
                     }
                     else if (nextPositionMoveTupleList.Last().Item1 != null)
-                    //The pseudo-legal move we're investigating does not allow a king to be captured. 
+                    // The pseudo-legal move we're investigating does not allow a king to be captured. 
                     {
                         if (depth == 0)
                         {
                             moveEvaluationTupleList.Add((move, evaluationFunction.EvaluatePosition(position)));
-                            //Interface.ConsoleGraphics.DrawPosition(position);
                         }
                         else
                         {
                             moveEvaluationTupleList.Add((move, FindBestMove(ref evaluationFunction, position, nextPositionMoveTupleList, depth - 1, alpha, beta).Item2));
-                            //Interface.ConsoleGraphics.DrawPosition(position);
-                        }
-                        //Console.WriteLine(String.Format("{0}The value we're adding to {1}, {2} to {3}, {4} is {5}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY, moveEvaluationTupleList.Last().Item2));
+                        }                        
                     }
-                    //MINIMAX
+                    // MINIMAX
                     if (moveEvaluationTupleList.Count != 0)
                     {
                         bestEvaluation = Math.Min(bestEvaluation, moveEvaluationTupleList.Last().Item2);
@@ -124,15 +105,11 @@ namespace Chess
                 }
                 if (moveEvaluationTupleList.Count == 0)
                 {
-                    //This only occurs when we're in a position where there are pseudo-legal moves, but no legal moves.
-                    //In this case, it's safe to return a random move and the evaluation for the final position.
+                    // This only occurs when we're in a position where there are pseudo-legal moves, but no legal moves.
+                    // In this case, it's safe to return a random move and the evaluation for the final position.
                     return (positionMoveTupleList.Last().Item2, evaluationFunction.EvaluatePosition(currentPosition));
                 }
                 moveEvaluationTupleList.Sort((x, y) => x.Item2.CompareTo(y.Item2));
-            }
-            foreach ((Move move, float evaluation) in moveEvaluationTupleList)
-            {
-                //Console.WriteLine(String.Format("{0}The move {1},{2} to {3},{4} has an evaluation of {5}", new String('\t', 1 - depth), move.fromX, move.fromY, move.toX, move.toY, evaluation));
             }
             return moveEvaluationTupleList[0];
         }
